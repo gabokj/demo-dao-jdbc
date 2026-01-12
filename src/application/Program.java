4dd5ed86b558b8312660d6c57c46f410c2bbf2e0
Gabriel Teixeira Ricca <gabriel.teixeira.ricca@ibm.com>
2026-01-12 15:29:00 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,5 +35,10 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserido! novo id = " +newSeller.getId());
 		
+		System.out.println("\n=== Test 5: seller Update ===");
+		seller = sellerDao.findById(1);
+		seller.setName("Gabriel Ricca");
+		sellerDao.update(seller);
+		System.out.println("Update completed!");
 	}
 }
